b94d566c29d8e3fd831cc88e6b21d65d2dfb7fed
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-08-30 11:00:34 +0530

    verification process errors fix

--- App/Model/Process/verification_process.php
+++ App/Model/Process/verification_process.php
@@ -3,9 +3,9 @@
 use EligerBackend\Model\Classes\Connectors\DBConnector;
 use EligerBackend\Model\Classes\Users\User;
 
-if (isset($POST_["code"])) {
+if (isset($_POST["code"])) {
     $user = new User();
-    return $user->verify($POST_["code"], DBConnector::getConnection());
+    echo $user->verify($_POST["code"], DBConnector::getConnection());
 } else {
     echo 500;
 }
