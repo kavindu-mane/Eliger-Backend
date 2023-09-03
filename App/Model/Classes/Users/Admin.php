8f40d4ce62bff949e888c66dfdf9d52e02abe58c
Kavindu Buddhika <cst20064@std.uwu.ac.lk>
2023-09-03 22:13:53 +0530

    accounts table loading backend

--- App/Model/Classes/Users/Admin.php
+++ App/Model/Classes/Users/Admin.php
@@ -20,11 +20,19 @@ class Admin extends User
 
     //load Accounts
     public function loadAccountDetails($connection,$account_type,$status){
+        $query = "select * from customer_details where Account_Status=?";
+
+        if($account_type === "driver"){
+            $query = "select * from driver_details where Account_Status=?";
+
+        }elseif($account_type === "hands"){
+            $query = "select * from hands_details where Account_Status=?";
+        }elseif($account_type === "vehicle_owner"){
+            $query = "select * from vehicle_owner_details where Account_Status=?";
+        }
         try {
-                $query = "select * from account_details where Account_Status=? AND Account_Type =?";
                 $pstmt = $connection->prepare($query);
                 $pstmt->bindValue(1, $status);
-                $pstmt->bindValue(2, $account_type);
                 $pstmt->execute();
                 return $pstmt->fetchAll(PDO::FETCH_OBJ);
             } catch (PDOException $ex) {
