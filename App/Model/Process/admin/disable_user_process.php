f962ebd654d07083d20cfb9408c46c470b181e47
Kavindu Buddhika <cst20064@std.uwu.ac.lk>
2023-11-23 09:15:00 +0530

    final fixing

--- App/Model/Process/admin/disable_user_process.php
+++ App/Model/Process/admin/disable_user_process.php
@@ -3,7 +3,7 @@
 use EligerBackend\Model\Classes\Connectors\DBConnector;
 use EligerBackend\Model\Classes\Users\Admin;
 
-if (isset($_SESSION["user"]) && $_SESSION["user"]["role"] === "admin") {
+if (isset($_SESSION["user"]) && ($_SESSION["user"]["role"] === "admin"  || $_SESSION["user"]["role"] === "hands")) {
     if (isset($_POST["status"], $_POST["email"])) {
         $admin = new Admin();
         echo $admin->disableUser(DBConnector::getConnection(), $_POST["status"], $_POST["email"]);
